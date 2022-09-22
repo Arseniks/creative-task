afed9933993f92e2f8f44f49525c036d6ab263bf
Senya <senya.korotkov@gmail.com>
2022-09-22 20:37:26 +0300

    refactoring README.md and formatting to pep8

--- creativetask/settings.py
+++ creativetask/settings.py
@@ -19,7 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)ak@*a==2_scvca9#u+#no&!$^9byyqnr9r82fq^_0x(^@ujx8'
+SECRET_KEY = 'django-insecure-)ak@*a==2_scvca9#u+#no&!$^9byyqnr9r82fq^_0x(' \
+             '^@ujx8 '
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -74,16 +75,20 @@ WSGI_APPLICATION = 'creativetask.wsgi.application'
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation'
+                '.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation'
+                '.MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation'
+                '.CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation'
+                '.NumericPasswordValidator',
     },
 ]
 
